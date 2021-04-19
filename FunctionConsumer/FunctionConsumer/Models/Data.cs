de74b4aacefc738fffb4085b6f2b598db3ab05ed
Laura Martinez Klein <lmartinezklein@gmail.com>
2021-04-19 17:57:22 -0400

    Completo

---
+++ FunctionConsumer/FunctionConsumer/Models/Data.cs
@@ -0,0 +1,17 @@
+﻿
+namespace FunctionConsumer.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Data
+    {
+        [Key]
+        public string NameDevice { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime EventDate { get; set; }
+        [Required]
+        public string Event { get; set; }
+
+    }
+}
